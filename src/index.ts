be5b70bf35841d776912dda7c241be083712ae65
Vidyesh Churi <vidyesh95@gmail.com>
2026-02-15 10:31:06 +0530

    chore: Update default application port from 3000 to 8000.

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const debug = debugLib('qatoto-backend:server');
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '8000');
 app.set('port', port);
 
 /**
