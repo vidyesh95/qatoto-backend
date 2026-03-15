13499a76fbae9ad898a24244baa791a2bc26c88a
Vidyesh Churi <vidyesh95@gmail.com>
2026-03-15 17:59:18 +0530

    feat: Add initial authentication module using better-auth configured with an anonymous plugin.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import { betterAuth } from "better-auth";
+import { anonymous } from "better-auth/plugins"
 import { Pool } from "pg";
 
 export const auth = betterAuth({
     database: new Pool({
         // connection options
     }),
+    plugins: [
+        anonymous()
+    ]
 })
